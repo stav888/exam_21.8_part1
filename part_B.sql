d27b7a438c3fbd7dfa27f4fb409db60a455249d8
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-21 18:22:46 +0300

    Update part_B.sql

--- part_B.sql
+++ part_B.sql
@@ -1,1 +1,14 @@
-
+create table televisions(
+  id INT not NULL serial,
+  catalog_number INT not NULL UNIQUE,
+  brand TEXT not NULL,
+  model TEXT not NULL,
+  screen_size INT,
+  resultion TEXT,
+  price INT not NULL,
+  stock_quantity INT,
+  relese_year INT not NULL,
+  smart_tv TEXT,
+  os TEXT,
+  panel_type TEXT
+)
