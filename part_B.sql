6c0b0914b98c527c3e04d7780e4a3a93a21e845d
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-21 18:51:08 +0300

    Update part_B.sql

--- part_B.sql
+++ part_B.sql
@@ -1,26 +1,25 @@
-  create table televisions(
-  id serial not NULL PRIMARY KEY,
-  catalog_number INT not NULL UNIQUE,
-  brand TEXT not NULL,
-  model TEXT not NULL,
+CREATE TABLE televisions(
+  id SERIAL PRIMARY KEY,
+  catalog_number VARCHAR NOT NULL UNIQUE,
+  brand TEXT NOT NULL,
+  model TEXT NOT NULL,
   screen_size INT,
-  resoltion TEXT CHECK (resolution IN ('4K', '8K', 'Full HD')),
-  price DECIMAL not NULL,
+  resolution TEXT CHECK (resolution IN ('4K', '8K', 'Full HD')),
+  price DECIMAL NOT NULL,
   stock_quantity INT DEFAULT 0,
-  relesae_year INT not NULL,
+  release_year INT NOT NULL,
   smart_tv BOOLEAN DEFAULT FALSE,
   os TEXT NULL,
   panel_type TEXT CHECK (panel_type IN ('OLED', 'QLED', 'LED'))
 );
 
-insert into televisions(catalog_number, brand, model, price, relese_year, ) VALUES()
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
+VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65, 'Full HD', 1199.99, 7, 2022, TRUE, 'MI', 'QLED');
 
 INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65.00, 'Full HD', 1199.99, 10, 2022, TRUE, 'MI', 'QLED');
+VALUES ('TV2', 'Samsung', 'KV80C', 80, '4K', 1600.00, 3, 2023, TRUE, 'Tizen', 'LED');
 
 INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV2', 'Samsung', 'KV80C', 80.00, '4K', 1600.00, 10, 2023, TRUE, 'Tizen', 'LED');
+VALUES ('TV3', 'Hisense', 'R76DC', 90, '8K', 2200.00, 10, 2025, FALSE, 'Android', 'OLED');
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV3', 'Hisense', 'R76DC', 90.00, '8K', 2200.00, 10, 2025, TRUE, 'Android', 'OLED');
-
+select * from televisions
