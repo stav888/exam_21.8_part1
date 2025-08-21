6abb7928d05e9d3c3255a7f18fce77e6eaef68fd
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-21 19:48:48 +0300

    Update part_B.sql

--- part_B.sql
+++ part_B.sql
@@ -16,25 +16,31 @@ CREATE TABLE storage(
   television_id INT NOT NULL,
   stock_quantity INT DEFAULT 0,
   price DECIMAL NOT NULL,
+  release_year INT NOT NULL,
+  smart_tv BOOLEAN DEFAULT FALSE,
   FOREIGN KEY (television_id) REFERENCES televisions(id) ON DELETE CASCADE
 );
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, release_year, smart_tv, os, panel_type)
-VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65, 'Full HD', 2022, TRUE, 'MI', 'QLED');
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, os, panel_type)
+VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65, 'Full HD', 'MI', 'QLED');
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, release_year, smart_tv, os, panel_type)
-VALUES ('TV2', 'Samsung', 'KV80C', 80, '4K', 2023, TRUE, 'Tizen', 'LED');
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, os, panel_type)
+VALUES ('TV2', 'Samsung', 'KV80C', 80, '4K', 'Tizen', 'LED');
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, release_year, smart_tv, os, panel_type)
-VALUES ('TV3', 'Hisense', 'R76DC', 90, '8K', 2025, FALSE, 'Android', 'OLED');
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, os, panel_type)
+VALUES ('TV3', 'Hisense', 'R76DC', 90, '8K', 'Android', 'OLED');
 
-INSERT INTO storage (television_id, stock_quantity, price)
-VALUES (1, 9, 1199.99);
+INSERT INTO storage (television_id, stock_quantity, price, release_year, smart_tv)
+VALUES (1, 9, 1199.99, 2022, TRUE);
 
-INSERT INTO storage (television_id, stock_quantity, price)
-VALUES (2, 4, 1600.00);
+INSERT INTO storage (television_id, stock_quantity, price, release_year, smart_tv)
+VALUES (2, 4, 1600.00, 2023, TRUE);
 
-INSERT INTO storage (television_id, stock_quantity, price)
-VALUES (3, 3, 2200.00);
+INSERT INTO storage (television_id, stock_quantity, price, release_year, smart_tv)
+VALUES (3, 3, 2200.00, 2025, FALSE);
 
 select * from televisions
+
+SELECT t.*, S.*
+FROM televisions t
+INNER JOIN storage s ON t.id = s.television_id;
