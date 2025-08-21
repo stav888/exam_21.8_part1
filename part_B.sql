bd4044a46c301d101c5fbb576b46daf34dc68ad7
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-21 19:22:03 +0300

    Update part_B.sql

--- part_B.sql
+++ part_B.sql
@@ -13,13 +13,29 @@ CREATE TABLE televisions(
   panel_type TEXT CHECK (panel_type IN ('OLED', 'QLED', 'LED'))
 );
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65, 'Full HD', 1199.99, 7, 2022, TRUE, 'MI', 'QLED');
+CREATE TABLE storage(
+  id SERIAL PRIMARY KEY,
+  television_id INT NOT NULL,
+  stock_units INT DEFAULT 0,
+  FOREIGN KEY (television_id) REFERENCES televisions(id) ON DELETE CASCADE
+);
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV2', 'Samsung', 'KV80C', 80, '4K', 1600.00, 3, 2023, TRUE, 'Tizen', 'LED');
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, release_year, smart_tv, os, panel_type)
+VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65, 'Full HD', 1199.99, 2022, TRUE, 'MI', 'QLED');
 
-INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV3', 'Hisense', 'R76DC', 90, '8K', 2200.00, 10, 2025, FALSE, 'Android', 'OLED');
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, release_year, smart_tv, os, panel_type)
+VALUES ('TV2', 'Samsung', 'KV80C', 80, '4K', 1600.00, 2023, TRUE, 'Tizen', 'LED');
+
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, release_year, smart_tv, os, panel_type)
+VALUES ('TV3', 'Hisense', 'R76DC', 90, '8K', 2200.00, 2025, FALSE, 'Android', 'OLED');
+
+INSERT INTO storage (television_id, stock_units)
+VALUES (1, 9);
+
+INSERT INTO storage (television_id, stock_units)
+VALUES (2, 4);
+
+INSERT INTO storage (television_id, stock_units)
+VALUES (3, 3);
 
 select * from televisions
