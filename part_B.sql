2ad478b88541597cb30502c39602d859749b4391
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-21 18:33:11 +0300

    Update part_B.sql

--- part_B.sql
+++ part_B.sql
@@ -1,14 +1,19 @@
 create table televisions(
-  id INT not NULL serial,
+  id INT serial not NULL PRIMARY KEY,
   catalog_number INT not NULL UNIQUE,
   brand TEXT not NULL,
   model TEXT not NULL,
   screen_size INT,
-  resultion TEXT,
+  resultion TEXT CHECK (resolution IN ('4K', '8K', 'Full HD')),
   price INT not NULL,
   stock_quantity INT,
   relese_year INT not NULL,
-  smart_tv TEXT,
-  os TEXT,
-  panel_type TEXT
+  smart_tv TEXT NULL,
+  os TEXT NULL,
+  panel_type TEXT CHECK (panel_type IN ('OLED', 'QLED', 'LED'))
 )
+
+insert into televisions(catalog_number, brand, model, price, relese_year, ) VALUES()
+
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
+VALUES ('TV001', 'Samsung', 'QN90C', 65.00, '4K', 1499.99, 10, 2023, TRUE, 'Tizen', 'QLED');
