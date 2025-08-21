484d512333f56aafd107e7d5b998eb1e6a499cbf
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-21 18:39:50 +0300

    Update part_B.sql

--- part_B.sql
+++ part_B.sql
@@ -16,4 +16,11 @@
 insert into televisions(catalog_number, brand, model, price, relese_year, ) VALUES()
 
 INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
-VALUES ('TV001', 'Samsung', 'QN90C', 65.00, '4K', 1499.99, 10, 2023, TRUE, 'Tizen', 'QLED');
+VALUES ('TV1', 'Xiaomi', 'SQ32CR', 65.00, 'Full HD', 1199.99, 10, 2022, TRUE, 'MI', 'QLED');
+
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
+VALUES ('TV2', 'Samsung', 'KV80C', 80.00, '4K', 1600.00, 10, 2023, TRUE, 'Tizen', 'LED');
+
+INSERT INTO televisions (catalog_number, brand, model, screen_size, resolution, price, stock_quantity, release_year, smart_tv, os, panel_type)
+VALUES ('TV3', 'Hisense', 'R76DC', 90.00, '8K', 2200.00, 10, 2025, TRUE, 'Android', 'OLED');
+
